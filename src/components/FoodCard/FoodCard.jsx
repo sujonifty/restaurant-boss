35bd020ac474dfc8416a9eb0a0e4d7d81715053a
sujonifty <sujonifty@gmail.com>
2024-05-27 18:07:13 +0600

    responsive updated

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,8 +1,52 @@
+import { useContext } from "react";
+import { authContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
-const FoodCard = ({item}) => {
-    const {name, image, price, recipe} = item;
-    const handleFood=(food)=>{
-        console.log(food)
+const FoodCard = ({ item }) => {
+    const { name, image, price, recipe, _id } = item;
+    const { user } = useContext(authContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const axiosSecure = useAxiosSecure();
+    const handleFood = (food) => {
+        if (user && user?.email) {
+            const cartItem = {
+                manuId: _id,
+                email: user.email,
+                name,
+                image,
+                price
+            }
+            axiosSecure.post('/carts', cartItem)
+            .then(res=>{
+                console.log(res.data);
+                if(res.data.insertedId){
+                    Swal.fire({
+                        icon: "success",
+                        title: `${name} added to cart`,
+                        showConfirmButton: false,
+                        timer: 2000
+                    });
+                }
+            })
+        }
+        else {
+            Swal.fire({
+                title: " You are not login",
+                text: "Please login to add to the cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, Login!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: { from: location } });
+                }
+            });
+        }
     }
 
     return (
@@ -13,7 +57,7 @@ const FoodCard = ({item}) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-center">
-                <button onClick={()=>handleFood(item)} className="uppercase btn btn-outline border-0 border-b-4 mt-4">Add Cart</button>
+                    <button onClick={() => handleFood(item)} className="uppercase btn btn-outline border-0 border-b-4 mt-4">Add Cart</button>
                 </div>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,21 @@ import { RouterProvider } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './Provider/AuthProvider.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <AuthProvider>
+          <div className='max-w-screen-xl mx-auto'>
+            <RouterProvider router={router} />
+          </div>
+        </AuthProvider>
+      </HelmetProvider>
+    </QueryClientProvider>
 
-    <HelmetProvider>
-      <AuthProvider>
-        <div className='max-w-screen-xl mx-auto'>
-          <RouterProvider router={router} />
-        </div>
-      </AuthProvider>
-    </HelmetProvider>
   </React.StrictMode>,
 )
