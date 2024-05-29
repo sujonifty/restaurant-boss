bfc7901028f5af99b9fa56d85425235fd1a802a1
sujonifty <sujonifty@gmail.com>
2024-05-29 12:29:24 +0600

    completed user delete section

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,17 +1,45 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const { data: users=[] } = useQuery({
+    const { data: users=[], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('users');
                 return res.data;
         }
     })
+
+    const handleDelete=(user)=>{
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              axiosSecure.delete(`/users/${user._id}`)
+              .then(res=>{
+                if(res.data.deletedCount > 0){
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your food item has been deleted.",
+                        icon: "success"
+                      });
+                      refetch();
+                }
+              })
+            }
+          });
+    }
     return (
         <div>
             <div className="flex justify-between my-4 p-5">
@@ -50,7 +78,7 @@ const AllUsers = () => {
                     </div>
                     </td>
                     <th>
-                      <button  className="btn btn-ghost">
+                      <button onClick={()=>handleDelete(user)} className="btn btn-ghost">
                         <FaTrashAlt className="text-red-700"></FaTrashAlt>
                       </button>
                     </th>
